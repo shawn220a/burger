4c2ec11b8e0e008add8d534f9742e819968744ed
shawn220a <shawn.42496@gmail.com>
2020-03-14 23:27:53 -0500

    Fix database

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 let connection;
 
-if (process.env.JAWS_URL) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
