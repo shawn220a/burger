67eecfdcd424122ed79392fc58f4441c327f7dbe
shawn220a <shawn.42496@gmail.com>
2020-03-14 22:04:55 -0500

    Add delete function to program

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,11 +2,9 @@
 $(function() {
   $(".change-devoured").on("click", function(event) {
     var id = $(this).data("id");
-    var newDevour = (!$(this).data("devoured"));
-    
+    var newDevour = !$(this).data("devoured");
+
     console.log(newDevour);
-    
-    
 
     var newDevouredState = {
       devoured: newDevour
@@ -18,13 +16,11 @@ $(function() {
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newDevouredState
-    }).then(
-      function() {
-        console.log("changed sleep to", newDevour);
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+    }).then(function() {
+      console.log("changed sleep to", newDevour);
+      // Reload the page to get the updated list
+      location.reload();
+    });
   });
 
   $(".create-form").on("submit", function(event) {
@@ -32,7 +28,9 @@ $(function() {
     event.preventDefault();
 
     var newBurger = {
-      burger_name: $("#ca").val().trim()
+      burger_name: $("#ca")
+        .val()
+        .trim()
     };
     console.log(newBurger);
 
@@ -40,12 +38,23 @@ $(function() {
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
-    }).then(
-      function() {
-        console.log("created new burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
+    }).then(function() {
+      console.log("created new burger");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $(".delete-burger").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(function() {
+      console.log("deleted burger", id);
+      // Reload the page to get the updated list
+      location.reload();
+    });
   });
 });
